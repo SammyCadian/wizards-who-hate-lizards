3d9b7adbbabb0b63e709943a958c981ad02cccf6
SammyCadian <samyst05@hotmail.com>
2025-03-13 13:14:00 -0400

    Fixed Resource Timer
    Resource income fixed
    Work on buttons for various amounts or types of units/abilities begun

--- Scenes/UI/battle_ui.gd
+++ Scenes/UI/battle_ui.gd
@@ -1,12 +1,18 @@
 extends Control
 
-@export var incomeTime = 5
+@export var incomeTime = 5.0
 
-var magicPoints
+var magicPoints = 0
+
+#Holds IDs for units and abilities
+var units = []
+var abilities = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ResourceTimer.wait_time = incomeTime/100
+	$IncomeTimer.set_wait_time(incomeTime/100)
+	$IncomeTimer.start()
+	updatePoints()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +22,11 @@ func _process(delta: float) -> void:
 func _on_income_timer_timeout() -> void:
 	if($IncomeVisual.value == 100):
 		$IncomeVisual.value = 0
-		magicPoints += 1
+		magicPoints += 10
+		updatePoints()
 	
 	$IncomeVisual.value += 1
 	pass # Replace with function body.
+
+func updatePoints():
+	$PointsLabel.text = str(magicPoints)
