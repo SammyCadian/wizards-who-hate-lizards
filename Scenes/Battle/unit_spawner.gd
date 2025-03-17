5b0596fbc03df025e21b34fa7e7fb7160606b5cd
Anna <128635799+anna0range@users.noreply.github.com>
2025-03-16 20:28:24 -0400

    Linked battle to spawner with signal

--- Scenes/Battle/unit_spawner.gd
+++ Scenes/Battle/unit_spawner.gd
@@ -6,56 +6,59 @@ extends Node2D
 var starting_enemies:int # How many enemies we start with
 var current_enemies: int # How many enemies there are currently
 
-var start_wave: int
-var current_wave: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Initializes wave counters, doesnt actually do anything yet
-	start_wave = 0
-	current_wave = start_wave
+	# Manually connect "laneSelected" signal from Battle node to this(Unit Spawner) node
+	var battle = get_node("/root/Game/Display Manager/Battle")
+	battle.laneSelected.connect(spawn)
 	
+	
+	# Initialize enemy counter
 	starting_enemies = get_child_count() # Typically 0 for now
 	current_enemies = get_child_count()
 
 
+
+
 # This loop will function as an enemy AI. It will handle waves
 func _process(delta: float) -> void:
+	# Checks if there are any enemies left
 	current_enemies = get_child_count()
-	if(starting_enemies == current_enemies):
-		current_wave += 1
-		# New wave animation/notification
-		print(current_wave)
-		enemy_spawn("spike", 1.5)
-		
-		
-
-# This function takes in the base spawns for that mob type, and will later be
-# modular as to increase spawns based on the level in the overwold and/or
-# the current wave
-func enemy_spawn(type, base_amount):
-	# Delay per spawn
-	var mob_spawn_delay: float = 2.0 
 	
-	# Calculates how many rounds to spawn based on current wave
-	var mob_rounds = base_amount*current_wave
-	if type == "spike":
-		var lane_1 = $EnemySpawnPoint1
-		var lane_2 = $EnemySpawnPoint2
-		var lane_3 = $EnemySpawnPoint3
-		if mob_rounds>=1:
-			for i in mob_rounds:
-				var spike1 = spike_scene.instantiate()
-				spike1.global_position = lane_1.global_position
-				var spike2 = spike_scene.instantiate()
-				spike2.global_position = lane_2.global_position
-				var spike3 = spike_scene.instantiate()
-				spike3.global_position = lane_3.global_position
-				
-				add_child(spike1)
-				await get_tree().create_timer(mob_spawn_delay).timeout
-				add_child(spike2)
-				await get_tree().create_timer(mob_spawn_delay).timeout
-				add_child(spike3)
-				await get_tree().create_timer(mob_spawn_delay).timeout
-				
+	# If there aren't any enemies left, spawns more
+	if(starting_enemies == current_enemies):
+		print("spawned spike")
+		enemy_spawn_prep($EnemySpawnPoint1, 2)
+		enemy_spawn_prep($EnemySpawnPoint2, 2)
+		enemy_spawn_prep($EnemySpawnPoint3, 2)
+
+
+
+
+# Will randomize the type of enemy spawned
+func enemy_spawn_prep(location, amount):
+	var randomType = randi_range(0, 1)
+	match randomType:
+		0:
+			spawn("spike_scene", location, amount)
+		1:
+			spawn("sus_scene", location, amount)
+
+
+
+
+
+# General spawner called by enemy AI and battle script
+func spawn(type:String, location, amount):
+	
+	
+	if(type == "spike_scene" || type == "sus_scene"):
+		# Loops through spawner "amount" times
+		for i in amount:
+			var unit = type.instantiate()
+			unit.global_position = location.global_position
+			add_child(unit)
+			await get_tree().create_timer(1.0).timeout
+	else:
+		print("Spawned in " + type)
--- Scenes/Lizards/spike_scene.gd
+++ Scenes/Lizards/spike_scene.gd
@@ -1,17 +1,12 @@
 extends Node2D
-@export var speed = -200 #Enemy speed
-var attacking
+@export var speed = -150 #Enemy speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Placeholder defaults
-	attacking = false
 	$AnimatedSprite2D.animation = "walk"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Starts walking and plays animation when instantiated
-	if (!attacking):
-		$AnimatedSprite2D.play()
-		position.x += round(speed*delta)
-		
+	$AnimatedSprite2D.play()
+	position.x += round(speed*delta)
