8c115432c45c0e2e762d1bc26299574552914a23
Anna <128635799+anna0range@users.noreply.github.com>
2025-04-05 19:38:12 -0400

    Fixed unit select crashes

---
+++ Scenes/Units/Lizards/ghost_scene.gd
@@ -0,0 +1,91 @@
+extends CharacterBody2D
+
+@export var damage = 1
+@onready var see_enemy = false
+var health = 30
+var damageTaken = 0
+#var isDead = false
+var deathTimer = 0
+
+var is_friendly = false
+
+var targets = []
+
+func _ready() -> void:
+	$AnimatedSprite2D.animation = "walk"
+	$AnimatedSprite2D.play()
+	z_index = global_position.y + 200
+	
+	
+func _process(delta: float) -> void:
+	if(deathTimer > 0):
+		$AnimatedSprite2D.animation = "death"
+		deathTimer -= delta
+		if(deathTimer < 0.5):
+			queue_free()
+	else:
+		move()
+
+
+func move():
+	if see_enemy:
+		velocity = Vector2(0, 0)   
+	elif !see_enemy:
+		velocity = Vector2(-50, 0)  
+	move_and_slide()
+
+
+func LizardDamage() -> int:
+	return damage
+
+
+func _on_damage_timer_timeout() -> void:
+	if(targets.size() > 0):
+		targets[0].takeDamage(damage)
+	#health -= damageTaken
+	#print("Scout health: " + str(health))
+	#if(health <= 0):
+		#$DamageTimer.stop()
+		#ouchieMyForehead()
+
+func takeDamage(damage: int):
+	health -= damage
+	if(health <= 0):
+		deathTimer <= 0
+		ouchieMyForehead()
+
+func _on_range_area_body_entered(body: Node2D) -> void:
+	targets.append(body)
+	$DamageTimer.start()
+	$AnimatedSprite2D.animation = "attack"
+	see_enemy = true
+
+
+func _on_range_area_body_exited(body: Node2D) -> void:
+	if(targets.has(body)):
+		targets.remove_at(targets.find(body))
+	if(targets.size() <= 0):
+		$DamageTimer.stop()
+		$AnimatedSprite2D.animation = "walk"
+		see_enemy = false
+
+func ouchieMyForehead():
+	deathTimer = 2.5
+	velocity = Vector2(0, 0)
+	if get_node("RangeArea") != null:
+		get_node("RangeArea").free()
+		get_node("HitboxArea").free()
+		get_node("CollisionShape2D").free()
+
+
+#func _on_hitbox_area_area_entered(area: Area2D) -> void:
+	#if area.get_parent().has_method("WizardDamage"):
+		#var node = area.get_parent() as Node
+		#damageTaken += node.WizardDamage()
+		#$DamageTimer.start()
+#
+#
+#func _on_hitbox_area_area_exited(area: Area2D) -> void:
+	#if area.get_parent().has_method("WizardDamage"):
+		#var node = area.get_parent() as Node
+		#damageTaken -= node.WizardDamage()
